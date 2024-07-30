fc729edb12d7d4944401737fa8cffbb1b81b17ef
Krzysztof <s26192@pjwstk.edu.pl>
2024-07-30 20:24:12 +0200

    moved classes in accordance with clean arch

---
+++ MallManager/MallManager.Client/Usecases/SimpleUsecase/DoTheThingHandler.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.Result;
+using Ardalis.SharedKernel;
+using Shared.Usecases.SampleUsecase;
+
+namespace MallManager.Client.Usecases.SimpleUsecase;
+
+public class DoTheThingHandler: ICommandHandler<DoTheThingCommand,Result<int>>
+{
+    public async Task<Result<int>> Handle(DoTheThingCommand request, CancellationToken cancellationToken)
+    {
+        if (request.I % 2 == 1)
+        {
+            return Result.NotFound();
+        }
+
+        return 2137;
+    }
+}
