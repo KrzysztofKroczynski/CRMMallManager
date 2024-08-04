dafb3e2523b8ce1a7fbaea7dda8fcfa7362b14aa
Daniłowski Cezary <s25901@pjwstk.edu.pl>
2024-08-04 18:40:18 +0200

    Added table for filtered apartments by apartment rental form

---
+++ MallManager/MallManager/Components/ResultOfFilteringComponent.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MallManager.Components;
+
+public partial class ResultOfFilteringComponent : ComponentBase
+{
+    // TODO: Place here business logic for loading locals
+
+    private string SelectedLocalId { get; set; }
+    
+    public List<ExampleModel> ExampleLocals = new List<ExampleModel>
+    {
+        new ExampleModel { Id = "XXXX", Localization = "P X L X ...", StartDate = DateTime.Now, EndDate = DateTime.Now },
+        new ExampleModel { Id = "YYYY", Localization = "P X L Y ...", StartDate = DateTime.Now, EndDate = DateTime.Now },
+        new ExampleModel { Id = "ZZZZ", Localization = "P X L Z ...", StartDate = DateTime.Now, EndDate = DateTime.Now }
+    };
+}
+
+public class ExampleModel
+{
+    public string Id { get; set; }
+    public string Localization { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
