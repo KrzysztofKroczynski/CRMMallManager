2df32f4ca3203c4c03198e234169b474888700a8
s25190 <s25190@pjwstk.edu.pl>
2024-08-05 20:14:36 +0200

    New Register screen

---
+++ MallManager/MallManager/Components/Pages/Register.razor.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+using MallManager.Components.Forms;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+
+namespace MallManager.Components.Pages;
+
+public partial class Register : ComponentBase
+{
+    bool success;
+    private RegistrationForm model = new();
+    private void OnValidSubmit(EditContext context)
+    {
+        success = true;
+        StateHasChanged();
+    }
+}
