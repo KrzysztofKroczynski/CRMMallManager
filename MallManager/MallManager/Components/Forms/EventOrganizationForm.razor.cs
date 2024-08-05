24c1938547100746c6932ad059763ab7ecc67c4b
Daniłowski Cezary <s25901@pjwstk.edu.pl>
2024-08-05 14:06:22 +0200

    Added event organization form

---
+++ MallManager/MallManager/Components/Forms/EventOrganizationForm.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MallManager.Components.Forms;
+
+public partial class EventOrganizationForm : ComponentBase
+{
+    private string? _eventName;
+    private DateTime? _startDate;
+    private DateTime? _endDate;
+    private string? _eventType;
+    private string? _description;
+    private int? _minMembersCount;
+    private int? _maxMembersCount;
+}
