91862fa62ac499e1c44c4d020c375f99f2eac079
Daniłowski Cezary <s25901@pjwstk.edu.pl>
2024-08-05 14:05:56 +0200

    Added advertising space rental form

---
+++ MallManager/MallManager/Components/Forms/AdvertisingSpaceRentalForm.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MallManager.Components.Forms;
+
+public partial class AdvertisingSpaceRentalForm : ComponentBase
+{
+    private List<string> months = new()
+    {
+        "Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec",
+        "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"
+    };
+
+    private string? _selectedMonth;
+    private bool? _isInTheCenter = false;
+    private bool? _isDuringPeakHours = true;
+    private string? _beneficiary;
+    private string? _description;
+    private bool? _isAnimated = false;
+    private bool? _replayOfThePreviousCampaign = false;
+    private bool? _durningWorkingDays = true;
+    private bool? _durningWeekend = true;
+}
