8e6ceadd6833ee3ba932238b630a5df213ecb9b7
Daniłowski Cezary <s25901@pjwstk.edu.pl>
2024-08-06 15:24:22 +0200

    Added menu component for logged manager

---
+++ MallManager/MallManager/Components/LoggedManagerMenu.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MallManager.Components;
+
+public partial class LoggedManagerMenu : LayoutComponentBase
+{
+    [Parameter]
+    public RenderFragment ChildContent { get; set; }
+    
+    bool _drawerOpen = false;
+
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+}
