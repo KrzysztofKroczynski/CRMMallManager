fe26ee18667c422ad8259d6fbda83fd09de4ba0e
Daniłowski Cezary <s25901@pjwstk.edu.pl>
2024-08-06 18:03:26 +0200

    Added guests requests, toggling form component for each of one, and small modifications

---
+++ MallManager/MallManager/Components/Forms/GuestRequestProcessingForm.razor.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MallManager.Components.Forms;
+
+public partial class GuestRequestProcessingForm : ComponentBase
+{
+    public string Message { get; set; }
+}
