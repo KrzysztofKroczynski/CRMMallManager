824ce6565eea393d29f24596c6539db07061a2de
Cezary Daniłowski <s25901@pjwstk.edu.pl>
2024-08-19 21:21:13 +0200

    Dynamic filtering of retail units based on values from the form has been added

--- MallManager/MallManager/Components/Forms/ApartmentRentalForm/ApartmentRentalForm.razor.cs
+++ MallManager/MallManager/Components/Forms/ApartmentRentalForm/ApartmentRentalForm.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.ComponentModel;
+using Ardalis.Specification;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Shared.Core.Entities;
 using Shared.Web.FormModels;
@@ -8,39 +10,93 @@ namespace MallManager.Components.Forms.ApartmentRentalForm;
 public partial class ApartmentRentalForm : ComponentBase
 {
     private RentalForm? _rentalForm = new();
+    
     private IEnumerable<RetailUnit> _allRetailUnits = Enumerable.Empty<RetailUnit>();
     private IEnumerable<RetailUnit> _filteredRetailUnits = Enumerable.Empty<RetailUnit>();
+    
+    private int _previousRetailUnitPurposeId = 0;
+    private int _previousSurfaceClassDictId = 0;
+    private DateTime? _previousStartDate = DateTime.Today;
+    private DateTime? _previousEndDate = DateTime.Today;
 
     protected override async Task OnInitializedAsync()
     {
         await RetailUnitService.LoadDataAsync();
         _allRetailUnits = RetailUnitService.RetailUnits;
         _filteredRetailUnits = _allRetailUnits;
+        _rentalForm.PropertyChanged += FormModelPropertyChanged;
     }
-
+    
+    private void FormModelPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        ApplyFilter();
+    }
+    
+    public void Dispose()
+    {
+        _rentalForm.PropertyChanged -= FormModelPropertyChanged;
+    }
+    
+    // TODO: Refactor this (perhaps move to service, or divide into smaller methods)
     private void ApplyFilter()
     {
-        /*Console.Beep();
-        if (RentalForm.SurfaceClassDict != null && RentalForm.RetailUnitPurpose != null && 
-            RentalForm.StartDate.HasValue && RentalForm.EndDate.HasValue)
+        _filteredRetailUnits = _allRetailUnits;
+        
+        if (_rentalForm.RetailUnitPurposeId != null && _rentalForm.RetailUnitPurposeId != _previousRetailUnitPurposeId)
         {
-            _filteredRetailUnits = RetailUnitService.FilterRetailUnits(
-                RentalForm.SurfaceClassDict,
-                RentalForm.RetailUnitPurpose,
-                DateOnly.FromDateTime(RentalForm.StartDate.Value),
-                DateOnly.FromDateTime(RentalForm.EndDate.Value)
-            );
-        }
-        else
-        {
-            _filteredRetailUnits = _allRetailUnits;
+            _filteredRetailUnits = _filteredRetailUnits.Where(retailUnit => 
+                retailUnit.RetailUnitPurposeId == _rentalForm.RetailUnitPurposeId)
+                .ToList();
+            
+            _previousRetailUnitPurposeId = _rentalForm.RetailUnitPurposeId;
         }
         
-        StateHasChanged();*/
+        if (_rentalForm.SurfaceClassDictId != null && _rentalForm.SurfaceClassDictId != _previousSurfaceClassDictId)
+        {
+            _filteredRetailUnits = _filteredRetailUnits.Where(retailUnit => 
+                    RetailUnitService.SurfaceClassDicts.Any(surfaceClassDict => 
+                        surfaceClassDict.Id == _rentalForm.SurfaceClassDictId 
+                        && retailUnit.LocalSurfaceArea >= surfaceClassDict.MinimalSurface 
+                        && retailUnit.LocalSurfaceArea <= surfaceClassDict.MaximumSurface)
+                    )
+                .ToList();
+            
+            _previousSurfaceClassDictId = _rentalForm.SurfaceClassDictId;
+        }
+
+        if (_rentalForm.StartDate != null && _rentalForm.StartDate != _previousStartDate)
+        {
+            var startDateAsDateOnly = new DateOnly?(DateOnly.FromDateTime(_rentalForm.StartDate.Value));
+            
+            _filteredRetailUnits = _filteredRetailUnits.Where(retailUnit =>
+                    RetailUnitService.GetAllLeasesForRetailUnitId(retailUnit.Id).Result.Where(lease =>
+                        startDateAsDateOnly < lease.EndDate).ToList()
+                    .Count == 0)
+                .ToList();
+
+            _previousStartDate = _rentalForm.StartDate;
+        }
+        
+        if (_rentalForm.EndDate != null && _rentalForm.EndDate != _previousEndDate)
+        {
+            var endDateAsDateOnly = new DateOnly?(DateOnly.FromDateTime(_rentalForm.EndDate.Value));
+            
+            _filteredRetailUnits = _filteredRetailUnits.Where(retailUnit =>
+                    RetailUnitService.GetAllLeasesForRetailUnitId(retailUnit.Id).Result.Where(lease =>
+                            endDateAsDateOnly > lease.StartDate).ToList()
+                        .Count == 0)
+                .ToList();
+
+            _previousEndDate = _rentalForm.EndDate;
+        }
+        
+        StateHasChanged();
     }
+    
 
     private void OnValidSubmit(EditContext context)
     {
+        // TODO: Add logic responsible for sending and navigating to convirmation page
         Console.WriteLine("Poprawny");
     }
 }
--- Shared/Web/FormModels/RentalForm.cs
+++ Shared/Web/FormModels/RentalForm.cs
@@ -1,37 +1,93 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Data;
-using MudBlazor;
-using MudBlazor.Extensions;
-using Shared.Core.Entities;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 namespace Shared.Web.FormModels;
 
-public class RentalForm : MudForm
+public class RentalForm : INotifyPropertyChanged
 {
+    private int _surfaceClassDictId;
+    private int _retailUnitPurposeId;
+    private DateTime? _startDate;
+    private DateTime? _endDate;
+    
     [Required(ErrorMessage = "Metraż jest wymagany")]
-    public string SurfaceClassDictName { get; set; } = string.Empty;
+    public int SurfaceClassDictId
+    {
+        get => _surfaceClassDictId;
+        set
+        {
+            if (_surfaceClassDictId != value)
+            {
+                _surfaceClassDictId = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     
     [Required(ErrorMessage = "Typ działalności jest wymagany")]
-    public string RetailUnitPurposeName { get; set; } = string.Empty;
+    public int RetailUnitPurposeId 
+    {
+        get => _retailUnitPurposeId;
+        set
+        {
+            if (_retailUnitPurposeId != value)
+            {
+                _retailUnitPurposeId = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     
     [Required(ErrorMessage = "Data \"od\" jest wymagana")]
     [DateEarlierThan("EndDate", ErrorMessage = "Data \"od\" musi być wcześniejsza niż data \"do\".")]
     [NotEarlierThanToday(ErrorMessage = "Data \"od\" nie może być wcześniejsza niż data dzisiejsza")]
-    public DateTime? StartDate { get; set; }
+    public DateTime? StartDate 
+    {
+        get => _startDate;
+        set
+        {
+            if (_startDate != value)
+            {
+                _startDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     
     [Required(ErrorMessage = "Data \"do\" jest wymagana")]
     [DateLaterThan("StartDate", ErrorMessage = "Data \"do\" musi być późniejsza niż data \"od\".")]
     [NotEarlierThanToday(ErrorMessage = "Data \"do\" nie może być wcześniejsza niż data dzisiejsza")]
-    public DateTime? EndDate { get; set; }
+    public DateTime? EndDate 
+    {
+        get => _endDate;
+        set
+        {
+            if (_endDate != value)
+            {
+                _endDate = value;
+                OnPropertyChanged();
+            }
+        }
+    }
     
     [Required(ErrorMessage = "Opis jest wymagany")]
     [StringLength(1000, MinimumLength = 1, ErrorMessage = "Maksymalnie może zawierać 1000 znaków")]
-    public string BusinessDescription { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Lokal jest wymagany")]
     public int RentalUnitId { get; set; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
 }
 
+
+// TODO: Perhaps move these validation attributes to some namespace
 public class DateLaterThanAttribute : ValidationAttribute
 {
     private readonly string _comparisonProperty;
