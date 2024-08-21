884972d719ea966d5e7ad0c6e5abaaefde540c60
Cezary Daniłowski <s25901@pjwstk.edu.pl>
2024-08-22 00:00:05 +0200

    Added repository with transactional adding new LeaseApplication

---
+++ MallManager/MallManager/Infrastructure/Persistence/LeaseApplicationRepository/ILeaseApplicationRepository.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Core.Entities;
+
+namespace MallManager.Infrastructure.Persistence.LeaseApplicationRepository;
+
+public interface ILeaseApplicationRepository
+{
+    public Task AddAsync(LeaseApplication leaseApplication, SurfaceClassDict surfaceClassDict,
+        RetailUnitPurpose retailUnitPurpose);
+}
